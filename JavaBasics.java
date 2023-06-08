eccf93aedbc4916ccffe011c88b28f2aa086e270
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-09 03:57:00 +0530

    hi

---
+++ JavaBasics.java
@@ -0,0 +1,34 @@
+import java.util.*;
+ public class JavaBasics{
+    public static void main (String args[]){
+Scanner sc = new Scanner(System.in);
+ int a = sc.nextInt();
+ int b = sc.nextInt();
+
+ char operator = sc.next().charAt(0);
+ switch(operator){
+    case '+' : {
+        System.out.println(a+b);
+    }
+    case '-' : { 
+        System.out.println(a-b);
+    }
+    case '*' : {
+    System.out.println(a*b);
+}
+   case '/' : {
+    System.out.println(a/b);
+   }
+   default : { 
+    System.out.println("wrong operator");
+   }
+
+ }
+
+
+
+
+
+
+    }
+ }
