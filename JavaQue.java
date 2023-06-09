552e0cd8bfe75890136e0eb4e3eb01e24ac18ca9
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-09 18:48:55 +0530

    ..

--- JavaQue.java
+++ JavaQue.java
@@ -2,13 +2,37 @@ import java.util.*;
 public class JavaQue{
     public static void main(String arg[]){
 Scanner sc = new Scanner(System.in);
-int a = sc.nextInt();
- if(a%2==0){
-    System.out.println("even");
- }
+int day = sc.nextInt();
+ switch(day){
+    case 1 : {
+        System.out.println("Monday");
+    }
+    case 2 : {
+        System.out.println("Tuesday");
+        break;
+    }
+    
+    case 3 : {
+        System.out.println("wednesday");
+        break;
+    }
+    case 4 : {
+        System.out.println("Thursady");
+        break;
+    }
+    case 5 : {
+        System.out.println("Friday");
+        break;
+    }
+    case 6 : {
+        System.out.println("Saturday");
+        break;
+    }
+    case 7 : {
+        System.out.println("Sunday");
 
- else {
-    System.out.println("odd");
+        break;
+    }
  }
     } 
 }
