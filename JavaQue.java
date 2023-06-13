7aa8c5d65416de1596b8e9b271ae810c39122f42
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-13 22:57:59 +0530

    ..

--- JavaQue.java
+++ JavaQue.java
@@ -1,22 +1,19 @@
-import java.util.*;
+
 public class JavaQue{
     public static void main(String arg[]){
-     Scanner sc = new Scanner(System.in);
     
-     
-
-     do{
-       System.out.print("Enter a number :");
-        int num = sc.nextInt() ;
-      if(num % 10 == 0){
-         break;
+    for(  int line =1 ; line<=4 ; line++ ){
+      for ( int star=1 ; star<= 4-line + 1 ; star++){
+         System.out.print("*");
+         
       }
-   System.out.println("The number was: " + num);
-      
-     }
-      
-      while(true);
- 
+      System.out.println();
+    }
+
+    
+    
+
+    
  
 
 
