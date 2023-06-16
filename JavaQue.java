0814b1c16dc48984d5aedcfa345883946051bd6f
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-16 21:40:09 +0530

    ..

--- JavaQue.java
+++ JavaQue.java
@@ -1,24 +1,20 @@
 
 public class JavaQue{
 
-public static int binomialCoeff(int n, int r){
-    int fact_n = factorial(n);
-    int fact_r = factorial(r);
-    int fact_nmr = factorial(n-r);
-    int binomialCoeff = fact_n / fact_r * fact_nmr ;
-    return binomialCoeff;
+public static boolean isPrime(int n){
+    boolean isPrime = true;
+ for(int i = 2 ;i<=n-1 ; i++){
+    if(i % n == 0){
+        isPrime = false;
+        break;
+    }
+ }
+return isPrime;
 }
 
-public static int factorial(int n){
-    int f = 1;
-    for(int i = 1; i<=n ; i++){
-        f = f* i;
-    }
-    return f ;
-}
     public static void main(String arg[]){
     
-    System.out.print(binomialCoeff(5,2));
+    System.out.print( isPrime(5));
     
 
     
