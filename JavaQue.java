5000bb57fbebf35957d26d79e041c6408128d705
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-18 03:30:53 +0530

    ..

--- JavaQue.java
+++ JavaQue.java
@@ -2,8 +2,10 @@
 public class JavaQue{
 
 public static boolean isPrime(int n){
-    boolean isPrime = true;
- for(int i = 2 ;i<=n-1 ; i++){
+  if(n==2){
+return true;
+}
+ for(int i = 2 ;i<= Math.sqrt(n) ; i++){
     if( n % i == 0){
         return false;
     }
