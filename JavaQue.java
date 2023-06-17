6783be2ab0c3fbff3c54484bb52c6408dfa9e61d
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-18 04:54:16 +0530

    ..

--- JavaQue.java
+++ JavaQue.java
@@ -2,10 +2,12 @@
 public class JavaQue{
 
 public static boolean isPrime(int n){
+// 1 is neither prime nor composite
   if(n==2){
 return true;
+
 }
- for(int i = 2 ;i<= Math.sqrt(n) ; i++){
+ for(int i = 2 ;i<= Math.sqrt(n) ; i++){ //optimized code
     if( n % i == 0){
         return false;
     }
@@ -14,9 +16,18 @@ return true;
 return true;
 }
 
+public static void primesInRange(int n){
+for(int i= 2 ; i <= n ; i++) {
+    if(isPrime(i)){
+        System.out.print(i + " ");
+    }
+}
+
+}
+
     public static void main(String args[]){
     
-    System.out.print( isPrime(22));
+    primesInRange(16);
     
 
     
