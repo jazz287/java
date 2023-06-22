8a7181282f2e02be6882ae7a343a94922f903708
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-22 23:50:55 +0530

    ..

--- JavaQue.java
+++ JavaQue.java
@@ -1,21 +1,19 @@
-import java.util.*;
+
 public class JavaQue{
 
-
-
-
+public static void arrayUpdate(int marks[]){
+ for(int i = 0 ; i < marks.length ; i++ ){
+  marks[i] = marks[i] + 1;
+ }
+}
 
 
 
     public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    int marks[] = new int[100];
-    marks[0]= sc.nextInt();
-    marks[1]= sc.nextInt();
-    marks[2]= sc.nextInt();
-
-    System.out.println("phy:" + marks);
-     System.out.println("chem:" + marks);
-      System.out.println("chem:" + marks);
+    int marks[] = {97,98,99};
+    arrayUpdate(marks);
+for(int i = 0 ; i < marks.length ; i++ ){
+System.out.println(marks[i]);
+}
     } 
 }
