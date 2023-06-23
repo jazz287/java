3b4ce16f4092516675dac898b8cf04adebf25b82
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-23 17:31:36 +0530

    ..

--- JavaQue.java
+++ JavaQue.java
@@ -1,19 +1,29 @@
 
 public class JavaQue{
-
-public static void arrayUpdate(int marks[]){
- for(int i = 0 ; i < marks.length ; i++ ){
-  marks[i] = marks[i] + 1;
+ //call by value
+public static int linearSearch(int numbers[],int key){
+ for (int i = 0 ; i <numbers.length ; i++){
+    if(numbers[i]==key){
+        return i ;
+    }
  }
-}
+  return -1;
+ 
+ }
+
 
 
 
     public static void main(String args[]){
-    int marks[] = {97,98,99};
-    arrayUpdate(marks);
-for(int i = 0 ; i < marks.length ; i++ ){
-System.out.println(marks[i]);
+        int numbers[] = {2,4,6,8,10,12,14};
+        int key = 10 ;
+        int index = linearSearch(numbers,key);
+
+if(index == -1){
+    System.out.println("key not found");
 }
+    else{
+        System.out.println("key is at :" + index);
+    }
     } 
 }
