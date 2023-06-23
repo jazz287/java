70412ad61b60320c7d019e0bd18ddaf8d04e4876
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-24 03:35:46 +0530

    ...

--- JavaQue.java
+++ JavaQue.java
@@ -1,29 +1,38 @@
-
+import java.util.* ;
 public class JavaQue{
- //call by value
-public static int linearSearch(int numbers[],int key){
- for (int i = 0 ; i <numbers.length ; i++){
-    if(numbers[i]==key){
-        return i ;
+ //BinarySearch
+public static int BinarySearch(int numbers[],int key){
+    int start = 0 ,end = numbers.length-1 ;
+    while(start <= end){
+        int mid = (start + end)/2 ;
+         if(key==numbers[mid]){
+            return mid;
+         }
+         if(key < numbers[mid]){
+            end = mid -1;
+         }
+         else{
+            start = mid +1;
+         }
     }
+    return -1 ;
+    
  }
-  return -1;
  
- }
+ 
+ 
 
 
 
 
     public static void main(String args[]){
-        int numbers[] = {2,4,6,8,10,12,14};
-        int key = 10 ;
-        int index = linearSearch(numbers,key);
+        int numbers[] = {4,6,2,8,10,12,14};
+        int key = 10;
+        int index = BinarySearch(numbers,key) ;
+        System.out.println("number is : " + index);
 
-if(index == -1){
-    System.out.println("key not found");
+
+
 }
-    else{
-        System.out.println("key is at :" + index);
-    }
-    } 
+ 
 }
