528b7e090526b4c7052c2d1f45033e8115a45167
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-26 19:31:22 +0530

    ...

--- JavaQue.java
+++ JavaQue.java
@@ -4,58 +4,55 @@ public class JavaQue{
 
 
 public static void subArrays(int numbers[]){
-   int tS = 0;
+ 
+   int max = Integer.MIN_VALUE;
+   int currSum = 0 ;
+   int prefix[] = new int[numbers.length];
+   prefix[0] = numbers[0];
+  
+   for(int i = 1 ; i < prefix.length ; i++){
+    prefix[i] = prefix[i-1] + numbers[i]; }
+  
    
-   for(int i = 0 ; i < numbers.length ; i++){
-    int start = i;
+
+   for(int i = 0 ; i< numbers.length ; i++){
+     int start = i;
+     
+
+   for(int j = i ; j < numbers.length ; j++ ){
+      int end = j ;
+
+      currSum = start == 0 ? prefix[end] : prefix[end]- prefix[start -1];
+   }
+     
+     if(currSum > max  ){
+         max = currSum;
+      }
+  
     
-   
-   for(int j = i ; j< numbers.length ; j++){
-    int end = j;
-    int Sum = 0;
-    for(int k = start ; k <= end ; k++){
-      Sum += numbers[k] ;
-      }
-       tS++;
-      for( int k = start ; k <= end ; k++ ){
-         System.out.print( numbers[k] + " ") ;
-         
-      }
 
-           System.out.println("sum is :" + Sum); 
-
+       }
+        
+        
+        
+      
+      
+       
+      
+        
+   System.out.println("max value is :" + max );
+        
+  
     }
      
+   
     
    
-    System.out.println();
    
   
-  
-    
-   }
    
    
-   System.out.print("total number of subArrays are :" + tS);
-   }
    
-   
-
-
-
-
-       
-    
-    
-    
- 
- 
- 
- 
-
-
-
-
     public static void main(String args[]){
         int numbers[] = {1,2,3,4,5,6,7,8};
 
