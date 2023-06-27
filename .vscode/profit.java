29fd3b7c2fb21cd0c6dc86331e4f74ff1c4ae85d
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-27 22:26:01 +0530

    ..

---
+++ .vscode/profit.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class profit{
+// findinf max profit for stocks
+public static void stocks(int Prices[]){
+
+int bP[] = new int[Prices.length];
+ bP[0] = Integer.MAX_VALUE ;
+
+
+int sP[] = new int[Prices.length];
+ sP[0] = 0 ;
+
+for(int i = 1 ; i < Prices.length ; i++){
+    sP[i] = Math.max(Prices[i] , Prices[i-1]);
+}
+
+for ( int i = 1 ; i<Prices.length ; i++){
+
+
+ bP[i] = Math.min(bP[i] , Prices[i-1]);
+ 
+}
+
+int profit[] = new int[Prices.length];
+ profit[0] = 0;
+int maxProfit = 0 ;
+for (int i = 1 ; i < Prices.length ; i++){
+     profit[i] = sP[i]- bP[i];
+    
+  maxProfit = Math.max(profit[i],profit[i-1]);
+}
+
+System.out.print("max profit is :" + maxProfit );
+
+
+}
+
+
+
+
+
+
+
+
+    public static void main (String args[]){
+
+ int Prices[] = {7,1,5,3,6,4};
+
+ stocks(Prices);
+    }
+}
