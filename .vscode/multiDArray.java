e7bd140b18b1b1f0fee4f87554df49f42207d096
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-06-29 05:33:14 +0530

    ..

--- .vscode/multiDArray.java
+++ .vscode/multiDArray.java
@@ -1,6 +1,19 @@
 import java.util.*;
 public class multiDArray{
 
+public static boolean Search(int matrix[][],int key,int n , int m){
+  for(int i = 0 ; i <n ; i++){
+    for(int j =0 ; j< m ; j++){
+        if(matrix[i][j]==key){
+        System.out.println("key found at"+ "(" + i +"," + j + ")");
+        return true ;
+        }  
+}
+  }
+  System.out.print("key not found");
+  return false;
+}
+
 public static void main (String args[]){
 int matrix[][] = new int [3][3];
 int n = matrix.length , m = matrix[0].length;
@@ -20,9 +33,7 @@ for(int i = 0 ; i<n ; i++){
     System.out.println();
 }
  
+ Search(matrix,5,n,m);
 
 }
-  
-
-    
 }
