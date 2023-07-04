4d9a25bca9570d3fd0411212eecbe3a97d03c7d6
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-07-05 02:07:42 +0530

    ways to declare strings

---
+++ .vscode/strings.java
@@ -0,0 +1,7 @@
+public class strings{
+    public static void main(String args[]){
+     char arr[] = {'a', 'b', 'c','d','e'};   
+     String str = "abcde" ;
+     String str2 = new String("xyz") ;
+    }
+}
