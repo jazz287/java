57cbdad81f4807697c2bcb10bd1d1bdd835e7219
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-07-06 03:59:07 +0530

    ..

--- .vscode/strings.java
+++ .vscode/strings.java
@@ -1,7 +1,32 @@
+import java.util.*;
 public class strings{
+public static void printStr(String str){
+
+for(int i = 0 ; i < str.length() ; i++){
+     System.out.print(str.charAt(i) + " ");
+}
+
+
+
+}
+
     public static void main(String args[]){
-     char arr[] = {'a', 'b', 'c','d','e'};   
+     /*char arr[] = {'a', 'b', 'c','d','e'};   
      String str = "abcde" ;
      String str2 = new String("xyz") ;
+
+     Scanner sc = new  Scanner(System.in);
+     String name = sc.nextLine();
+     System.out.println(name); */
+    
+    String name = "Jasmine";
+    String surname = "Wadhwa";
+    String fullname = name + " " + surname ;
+
+    // calling out the strings
+
+  printStr(fullname);
+   
     }
+
 }
